db65463d9b6a283f2f756068dc991aa5f9471fc6
Key2Board <81231376+Key2Board@users.noreply.github.com>
2022-05-10 19:02:58 +0200

    Commit Fran
    Se ha continuado el blockout del nivel 4, y se ha arreglado bugs

--- ArmaduraOxidada/Assets/Scripts/Triggers/Teleport.cs
+++ ArmaduraOxidada/Assets/Scripts/Triggers/Teleport.cs
@@ -6,12 +6,20 @@ public class Teleport : MonoBehaviour
 {
     public Transform teleportTarget;
     public GameObject checkpointLocation;
+    public AudioSource teleport;
+    private bool activado = false;
     //public Transform cameraTarget;
     //public GameObject cameraOrigin;
 
     void OnTriggerEnter(Collider collider)
     {
         checkpointLocation.transform.position = teleportTarget.transform.position;
+        if(activado == false)
+        {
+            teleport.Play();
+            activado = true;
+        }
+
         //cameraOrigin.transform.position = cameraTarget.transform.position;
     }
 }
