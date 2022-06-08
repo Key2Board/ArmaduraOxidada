7c72c0a703f59f9676710de0c6517b4a749ec84b
Key2Board <81231376+Key2Board@users.noreply.github.com>
2022-06-08 19:42:40 +0200

    Partículas y bugs
    Se ha añadido partículas al nivel 4 y se han arreglado algunos bugs

--- ArmaduraOxidada/Assets/Scripts/Pausar.cs
+++ ArmaduraOxidada/Assets/Scripts/Pausar.cs
@@ -10,6 +10,15 @@ public class Pausar : MonoBehaviour
     public GameObject pauseMenuUI;
     public GameObject Camera;
     public AudioSource Musica;
+    public AudioSource D1;
+    public AudioSource D2;
+    public AudioSource D3;
+    public AudioSource D4;
+    public AudioSource D5;
+    public AudioSource D6;
+    public AudioSource D7;
+    public AudioSource D8;
+    public AudioSource D9;
     public bool IsPaused { get { return Pausar.GameIsPaused; } }
 
     void Update()
@@ -31,6 +40,15 @@ public class Pausar : MonoBehaviour
     {
         pauseMenuUI.SetActive(false);
         Musica.Play();
+        /*D1.Play();
+        D2.Play();
+        D3.Play();
+        D4.Play();
+        D5.Play();
+        D6.Play();
+        D7.Play();
+        D8.Play();
+        D9.Play();*/
         Camera.GetComponent<SFPSC_FPSCamera>().enabled = true;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.None;
@@ -42,6 +60,15 @@ public class Pausar : MonoBehaviour
     {
         pauseMenuUI.SetActive(true);
         Musica.Pause();
+        /*D1.Pause();
+        D2.Pause();
+        D3.Pause();
+        D4.Pause();
+        D5.Pause();
+        D6.Pause();
+        D7.Pause();
+        D8.Pause();
+        D9.Pause();*/
         Camera.GetComponent<SFPSC_FPSCamera>().enabled = false;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
